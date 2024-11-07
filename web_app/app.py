f19453ff31154bc9806aa68928b71703294cf072
eran.zaksh <eranzaksh@gmail.com>
2024-11-07 10:46:55 +0200

    adding code

---
+++ web_app/app.py
@@ -0,0 +1,140 @@
+import os
+import time
+import logging
+<<<<<<< HEAD
+from flask import Flask, render_template, request, redirect, url_for
+=======
+
+import requests
+from flask import Flask, render_template, request, redirect, url_for, jsonify
+>>>>>>> c899afa (commit message)
+from prometheus_client import Histogram, make_wsgi_app, Counter
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from modules import backend
+
+app = Flask(__name__)
+
+logging.basicConfig(
+    level=logging.DEBUG,  # Set to DEBUG to capture all levels
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler("app.log"),  # Log to a file named 'app.log'
+        logging.StreamHandler()  # Also log to the console
+
+    ]
+)
+log = logging.getLogger('werkzeug')
+log.getLogger = True
+logging.getLogger('matplotlib').setLevel(logging.CRITICAL)
+logging.getLogger('urllib3').setLevel(logging.CRITICAL)
+
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
+REQUEST_COUNT = Counter(
+    'app_request_count',
+    'Application Request Count',
+    ['method', 'endpoint', 'http_status']
+)
+
+# REQUEST_LATENCY = Histogram(
+#     'app_request_latency_seconds',
+#     'Application Request Latency',
+#     ['method', 'endpoint']
+# )
+
+LOCATION_REQUEST_COUNT = Counter(
+    'location_request_count',
+    'Application Request Count',
+    ['method', 'endpoint', 'http_status', 'location']
+)
+
+@app.route("/download")
+def download():
+    backend.download_from_s3()
+    return redirect(url_for("home"))
+
+@app.route("/history/")
+def show_history():
+    files = backend.list_history()
+    return render_template('history.html', files=files)
+@app.route("/save_data/<string:location>")
+def save_data(location):
+    weather_json_file = backend.read_json_file(location)
+    backend.save_data_to_dynamo(weather_json_file)
+    return redirect(url_for("display", location=location))
+
+@app.route("/display/<string:location>", methods=["GET", "POST"])
+def display(location):
+    # start_time = time.time()
+    LOCATION_REQUEST_COUNT.labels(request.method, '/display', 200, location).inc()
+    logging.info(f"Display data for location: {location}")
+<<<<<<< HEAD
+    if request.method == "POST":
+        location = location.split(',')[0].lower()
+        return redirect(url_for("save_data", location=location))
+    # weather_json_file is a list of days and location
+=======
+>>>>>>> c899afa (commit message)
+    weather_json_file = backend.read_json_file(location)
+    backend.create_graph(weather_json_file[0])
+    graph_day = url_for('static', filename='graph_day.png')
+    graph_night = url_for('static', filename='graph_night.png')
+    logging.debug(f"Generated graphs for location: {location}")
+<<<<<<< HEAD
+=======
+    if request.method == "POST":
+        action = request.form.get("action")
+
+        if action == "send-email":
+            receiver_email = request.form['receiver_email']
+            email_service_url = "http://send-email-api:5001/send-email"
+            payload = {
+                "subject": location,
+                "body": weather_json_file[0],
+                "receiver_email": receiver_email
+            }
+            response = requests.post(email_service_url, json=payload)
+            if response.status_code != 200:
+                logging.info(f"Email failed {response.text}")
+                return render_template('error.html', error_message="Failed to send email", error_code=response.status_code)
+            if response.status_code == 200:
+                logging.info(f"Email sent succesfully {response.status_code}")
+                return render_template('display.html', weather=weather_json_file[0],
+                        location=weather_json_file[1], graph_day=graph_day, graph_night=graph_night)
+            
+            return render_template('display.html', weather=weather_json_file[0],
+                                   location=weather_json_file[1], graph_day=graph_day, graph_night=graph_night)
+        location = location.split(',')[0].lower()
+        return redirect(url_for("save_data", location=location))
+    # weather_json_file is a list of days and location
+    # weather_json_file = backend.read_json_file(location)
+>>>>>>> c899afa (commit message)
+    return render_template('display.html', weather=weather_json_file[0],
+                           location=weather_json_file[1], graph_day=graph_day, graph_night=graph_night)
+
+
+@app.route('/', methods=["POST", "GET"])
+def home():
+    REQUEST_COUNT.labels('GET', '/', 200).inc()
+    logging.info("Home endpoint was called")
+    bg_color = os.getenv('BG_COLOR', '#ffffff')
+    if request.method == "POST":
+        location = request.form.get("location").lower()
+        backend.save_history(location)
+        if backend.check_cache(location):
+            return redirect(url_for("display", location=location))
+        else:
+            raw_json = backend.get_api_with_user_input(location)
+            filtered_json = backend.filter_api(raw_json)
+            backend.create_json_file(filtered_json)
+            return redirect(url_for("display", location=filtered_json[1]))
+    return render_template('home.html', bg_color=bg_color)
+
+
+if __name__ == "__main__":
+<<<<<<< HEAD
+    app.run()
+=======
+    app.run(host="0.0.0.0", port=5001)
+>>>>>>> c899afa (commit message)
---
+++ web_app/modules/backend.py
@@ -0,0 +1,151 @@
+import datetime
+import os
+import logging
+import requests
+from deep_translator import GoogleTranslator
+import json
+from dotenv import load_dotenv, dotenv_values
+import matplotlib as mpl
+import matplotlib.pylab as plt
+from flask import abort
+from boto3.session import Session
+
+load_dotenv()
+cache = os.getenv("cache")
+<<<<<<< HEAD
+history_dir = os.getenv("history_dir")
+=======
+history_dir = "static/history"
+>>>>>>> c899afa (commit message)
+API_KEY = os.getenv("API_KEY")
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+AWS_DYNAMODB_ACCESS_KEY = "AWS_DYNAMODB_ACCESS_KEY"
+AWS_DYNAMODB_SECRET_KEY = "AWS_DYNAMODB_SECRET_KEY"
+
+
+
+def list_history():
+    files = [f for f in os.listdir(history_dir)]
+    return files
+
+
+def save_history(location):
+    date = str(datetime.datetime.today().date())
+    history_file = {"date": date, "city": location}
+    with open(f'{history_dir}/{location}.json', 'w') as fileobj:
+        json.dump(history_file, fileobj)
+
+
+
+def save_data_to_dynamo(weather_json_data):
+    session = Session(aws_access_key_id=AWS_ACCESS_KEY,
+                      aws_secret_access_key=AWS_SECRET_KEY,
+                      region_name='eu-north-1')
+
+    weather_json_data = json.dumps(weather_json_data)
+    new_dict = {}
+    database = session.resource('dynamodb')
+    table = database.Table('weather-app-database')
+    # "data" is the primary key of my table in dynamodb
+    new_dict["data"] = weather_json_data
+    table.put_item(Item=new_dict)
+
+
+def download_from_s3():
+    session = Session(aws_access_key_id=AWS_ACCESS_KEY,
+                      aws_secret_access_key=AWS_SECRET_KEY,
+                      region_name='eu-north-1')
+    s3 = session.resource('s3')
+    desktop = os.path.expanduser("~/Desktop")
+    s3.Bucket('my-static-web-site2').download_file('sky.jpg', f'{desktop}/my_local_sky.jpg')
+
+
+def create_graph(weather):
+    # showing graph of temps during the week.
+    mpl.use('agg')
+    days = []
+    day_temps = []
+    night_temps = []
+    for index in weather.values():
+        days.append(index['date'].split('-')[2])
+        day_temps.append(index['morning_temp'])
+        night_temps.append(index['evening_temp'])
+    plt.xlabel('Days', fontsize=15)
+    plt.ylabel('Morning Temps', fontsize=15)
+    plt.bar(days, day_temps, tick_label=days, color='lightblue')
+    plt.savefig('static/graph_day.png')
+    plt.xlabel('Days', fontsize=15)
+    plt.ylabel('Evening Temps', fontsize=15)
+    plt.bar(days, night_temps, tick_label=days, color='lightblue')
+    plt.savefig('static/graph_night.png')
+
+
+def check_cache(location):
+    # Check if folder has more then 10 files
+    files = os.listdir(cache)
+    files = [os.path.join(cache, x) for x in files]
+    files = sorted(files, key=os.path.getmtime)
+    if files:
+        oldest_file = files[0]
+        # Check if at least 10 files in folder - delete the oldest one.
+        if len(os.listdir("cache")) >= 11:
+            os.remove(oldest_file)
+    # Check cache for up-to-date location weather file
+    date_today = datetime.datetime.now()
+    for file in os.listdir(cache):
+        file_path = os.path.join(cache, file)
+        file_date = datetime.datetime.fromtimestamp(os.path.getctime(file_path))
+        # Check if file is older than today and delete it then continue to not return it
+        num = (date_today - file_date).days
+        if num >= 1:
+            os.remove(file_path)
+            continue
+        # if location in file.split('.')[0]
+        if file.split('.')[0] == location:
+            return True
+    return False
+
+
+def get_api_with_user_input(location):
+    # Get request to the api, if not 200 then a bad string was entered in the query
+    response = requests.request("GET",
+                                f'https://weather.visualcrossing.com/VisualCrossingWebServices/'
+                                f'rest/services/timeline/{location}/'
+                                f'?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Ctempmax%2Ctempmin%2Ctemp%2'
+                                f'Chumidity&include=days%2Chours&key={API_KEY}&contentType=json')
+
+    if response.status_code != 200:
+        logging.error(f"Error processing location {location}")
+        abort(400, "Need to enter a name")
+    json_data = response.json()
+    return json_data
+
+
+def filter_api(json_raw):
+    # Return days dict with filtered info and the city name + country
+    days = {}
+    city = json_raw['resolvedAddress']
+    translated_city = GoogleTranslator(source='auto', target='en').translate(city)
+    for i in range(7):
+        days[i] = {'date': json_raw["days"][i]['datetime'],
+                   'morning_temp': json_raw["days"][i]['hours'][7]['temp'],
+                   'morning_humidity': json_raw["days"][i]['hours'][7]['humidity'],
+                   'evening_temp': json_raw["days"][i]['hours'][19]['temp'],
+                   'evening_humidity': json_raw["days"][i]['hours'][19]['humidity']}
+    return days, translated_city
+
+
+def create_json_file(filtered_data):
+    # Create file name only from the simple name of the location for caching
+    path = filtered_data[1].split(',')[0].lower()
+    with open(f'{cache}/{path}.json', 'w') as fileobj:
+        json.dump(filtered_data, fileobj)
+
+
+def read_json_file(location):
+    # Return only the days dict, takes only the simple name as file name for caching
+    location = location.split(',')[0].lower()
+    with open(f'{cache}/{location}.json', 'r') as fileobj:
+        data = json.load(fileobj)
+    return data
